cf1b932de591047488b61fd0d16165fc03c1b054
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-09-06 21:08:20 +0200

    Update poweri.cpp

--- poweri.cpp
+++ poweri.cpp
@@ -2,31 +2,31 @@
 #include <list>
 #include <numeric>
 
-long long recursivePowi(unsigned int x, unsigned int y){
+uint64_t recursivePowi(uint32_t x, uint32_t y){
     if(!y) return 1;
     auto temp = recursivePowi(x, y/2);
     if(y % 2 == 0) return temp * temp;
     return temp * temp * x;
 }
 
-long long powi(unsigned int x, unsigned int y){
+uint64_t powi(uint32_t x, uint32_t y){
     if (!y) return 1;
-    std::list<long long> ls;
-    long long res = x;
+    std::list<uint64_t> ls;
+    uint64_t res = x;
     for(;y > 1; y/=2){
         if (y  % 2 == 1) ls.push_back( res );
         res *= res;
     }
-    return std::accumulate(ls.begin(), ls.end(), res, [](unsigned int l, unsigned int r){return l * r;}) ;
+    return std::accumulate(ls.begin(), ls.end(), res, [](uint64_t l, uint64_t r){return l * r;}) ;
 }
 
 int main(){
 
-    unsigned x = 0;
-    for(size_t j{}; j<32;++j) x+= powi(2,j);
+    uint64_t x = 0;
+    for(uint32_t j{}; j<32;++j) x+= powi(2,j);
 
-    unsigned recursiveX = 0;
-    for(size_t j{}; j<32;++j) recursiveX+= recursivePowi(2,j);
+    uint64_t recursiveX = 0;
+    for(uint32_t j{}; j<32;++j) recursiveX+= recursivePowi(2,j);
 
-    std::cout << powi(2, 32) - 1 << "\n" << x << " " << recursiveX << " " << std::numeric_limits<unsigned int>::max();
+    std::cout << powi(2, 32) - 1 << "\n" << x << " " << recursiveX << " " << std::numeric_limits<uint32_t>::max();
 }
